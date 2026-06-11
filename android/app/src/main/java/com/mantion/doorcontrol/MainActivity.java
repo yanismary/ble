73dc2f6ef3b751bf236fbfe66616c6b8032876ce
Yanis MARY <MaryY@SMT.domaine.local>
2026-06-11 10:41:13 +0200

    chore(android): cible le SDK 35

---
+++ android/app/src/main/java/com/mantion/doorcontrol/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mantion.doorcontrol;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ android/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mantio.app',
+  appName: 'Mantion SMT',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
