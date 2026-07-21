1572bd7b7d59ce14ed9b7cef515bb576ec268783
Yanis MARY <MaryY@SMT.domaine.local>
2026-07-21 08:44:43 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'V2.1',
+  webDir: 'www'
+};
+
+export default config;
